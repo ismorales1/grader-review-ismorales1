0b23d02fd6ba483b6a47b387bd2e1dffa84367d6
ismorales1 <122497830+ismorales1@users.noreply.github.com>
2023-03-11 18:14:22 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -4,7 +4,28 @@ rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
-cp student-submission/ListExamples.java ./
-javac -cp $CPATH *.java
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+cd student-submission
+if [[ -f ListExamples.java ]]
+    then 
+        echo "ListExamples found"
+    else 
+        echo "ListExamples not found"
+        exit 1
+fi
 
+#copied testing files to this directory
+cp ../TestListExamples.java ./
+cp -r ../lib ./
+
+javac -cp ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" *.java
+
+if [[ $? -eq 0 ]]
+    then 
+        echo "Program compiled"
+    else 
+        echo "Program did not compile"
+        exit 1
+fi
+
+java -cp ":lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > output.txt
+grep "Failures" output.txt
